d283090f28b95766439ec59b9a6ffd1d6aa18c37
Ben Andrews <ben@tapestryresearch.com>
2025-09-15 14:11:36 +0100

    printing

--- download_example.py
+++ download_example.py
@@ -1,5 +1,6 @@
 from api.forsta_api_utils import download_project_file
 from survey_elements.parsing.xml_parser import parse_survey
+from survey_elements.utils.xml_helpers import to_xml_string
 project_id = "module_sm"
 
 # Download (and save) the XML from Decipher
@@ -7,3 +8,6 @@ xml_root = download_project_file(f"/selfserve/2222/{project_id}", "xml")
 
 # Parse into Python objects
 survey = parse_survey(xml_root)
+
+for eachthing in survey:
+    print(to_xml_string(eachthing.to_xml_element(), pretty=True))
